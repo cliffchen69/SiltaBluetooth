ac24c4996cde75db611713e032fd697a2504aea1
Cliff <cliffchem@aengin.com>
2016-01-06 10:21:11 +0800

    first add

---
+++ Framework/SiltaBluetooth.framework/PrivateHeaders/SiltaBluetooth.h
@@ -0,0 +1,28 @@
+//
+//  SiltaBluetooth.h
+//  SiltaBluetooth
+//
+//  Created by LinYu Chan on 12/17/15.
+//  Copyright © 2015 aengin.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Siltabluetooth/GAI.h>
+#import <Siltabluetooth/GAIDictionaryBuilder.h>
+#import <Siltabluetooth/GAIEcommerceFields.h>
+#import <Siltabluetooth/GAIEcommerceProduct.h>
+#import <Siltabluetooth/GAIEcommerceProductAction.h>
+#import <Siltabluetooth/GAIEcommercePromotion.h>
+#import <Siltabluetooth/GAIFields.h>
+#import <Siltabluetooth/GAILogger.h>
+#import <Siltabluetooth/GAITrackedViewController.h>
+#import <Siltabluetooth/GAITracker.h>
+//! Project version number for SiltaBluetooth.
+FOUNDATION_EXPORT double SiltaBluetoothVersionNumber;
+
+//! Project version string for SiltaBluetooth.
+FOUNDATION_EXPORT const unsigned char SiltaBluetoothVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SiltaBluetooth/PublicHeader.h>
+
+
---
+++ SiltaBluetooth.podspec
@@ -0,0 +1,18 @@
+Pod::Spec.new do |s|
+  s.name         = "SiltaBluetooth"
+  s.version      = "0.1.0"
+  s.summary      = "SiltaBluetooth for easy to use BLE."
+  s.homepage     = "https://github.com/Giiixx/SiltaBluetooth"
+  s.license      = { :type => "MIT", :file => "LICENSE" }
+  s.author       = { "Cliff" => "cliffchem@aengin.com" }
+  s.platform     = :ios
+  s.ios.deployment_target = '8.0'
+  s.source       = { :git => "https://github.com/Giiixx/SiltaBluetooth.git", :tag => "#{s.version}" }
+  s.ios.preserve_paths  = 'SiltaBluetooth/*.framework'
+  s.ios.vendored_frameworks = 'SiltaBluetooth/SiltaBluetooth.framework'
+  s.ios.resource = 'SiltaBluetooth/SiltaBluetooth.framework'
+  s.ios.xcconfig = { 'LD_RUNPATH_SEARCH_PATHS' => '"$(PODS_ROOT)/SiltaBluetooth"' }
+  s.source_files = 'SiltaBluetooth/**/*.{h,m}'
+  s.requires_arc = true
+
+end
